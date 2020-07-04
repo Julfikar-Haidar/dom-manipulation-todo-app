b866d5a419b55530eb2040b18a0e62066d001701
Julfikar <julfikar6262@gmail.com>
2020-07-04 23:01:25 +0600

    clear,remove,filter task add

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const from = document.querySelector('#task-form')
 const tasklist = document.querySelector('.collection')
 const taskInput = document.querySelector('#task')
-
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
 // load all event
 
 loadEventListener()
 
 function loadEventListener(){
+    // add Task 
     from.addEventListener('submit', addTask)
+
+    // remove addlistener
+    tasklist.addEventListener('click', removeTask)
+
+    // claer all task
+    clearBtn.addEventListener('click', clearTask)
+
+    // filter task
+    filter.addEventListener('keyup', filterTask)
+
+
 }
 
 // add task
@@ -30,4 +43,37 @@ function addTask(e) {
     taskInput.value = ''
 
     e.preventDefault()
+}
+
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure ?')){
+            e.target.parentElement.parentElement.remove()
+
+        }
+    }
+// console.log(e.target);
+
+}
+
+
+function clearTask(e){
+   tasklist.innerHTML = ''
+    e.preventDefault()
+}
+
+
+function filterTask(e) {
+    const text = e.target.value.toLowerCase()
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent
+        if(item.toLocaleLowerCase().indexOf(text) !=-1){
+            task.style.display = 'block'
+        }
+        else{
+            task.style.display = 'none'
+        }
+    })
+    console.log(text);
+    
 }
